b18c86c45509da1cb6203257b2c082f71c83abd4
yano-kentaro <ynkn0829@gmail.com>
2021-01-19 12:37:07 +0900

    ajax-appの作り直し

---
+++ app/javascript/checked.js
@@ -0,0 +1,25 @@
+function check() {
+  const posts = document.querySelectorAll(".post");
+  posts.forEach(function (post) {
+    post.addEventListener("click", () => {
+      const postId = post.getAttribute("data-id");
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET", `/posts/${postId}`, true);
+      XHR.responseType = "json";
+      XHR.send();
+      XHR.onload = () => {
+        if (XHR.status != 200) {
+          alert(`Error ${XHR.status}: ${XHR.statusText}`);
+          return null;          
+        }
+        const item = XHR.response.post;
+        if (item.checked === true) {
+          post.setAttribute("data-check", "true");
+        } else if (item.checked === false) {
+          post.removeAttribute("data-check");
+        }
+      };
+    });
+  });
+}
+setInterval(check, 1000);
